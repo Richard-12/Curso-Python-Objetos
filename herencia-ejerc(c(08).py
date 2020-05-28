633a106eabca6e73782dd776b7e8a6d115308f16
Richard <rdiazgf@gmail.com>
2020-05-27 20:18:13 -0400

    Conjetura3

--- herencia-ejerc(c(08).py
+++ herencia-ejerc(c(08).py
@@ -14,14 +14,23 @@ class op_basicas(Calculadora):
     def suma(self):
         a,b,=self.datos
         s=a+b
-        print('El resultado es: ',s)
+        print('El resultado de la suma es: ',s)
 
     def resta(self):
         a,b,=self.datos
         r=a-b
-        print('El resultado es: ',r)
+        print('El resultado de la resta es: ',r)
 
+    def producto(self):
+        a,b,=self.datos
+        p=a*b
+        print('El resultado del producto  es: ',p)
 
+    def potencia(self):
+        a, b, = self.datos
+        pot = a ** b
+        print('El resultado de la potencia  es: ', pot)
+#Segunda subclase
 class raiz(Calculadora):
     def __init__(self):
         Calculadora.__init__(self,1)
@@ -29,13 +38,19 @@ class raiz(Calculadora):
     def cuadrada(self):
         import math
         a, = self.datos
-        print('El resultado es: ',math.sqrt(a))
+        print('El resultado de la raiz cuadrada  es: ',math.sqrt(a))
 
-#ejemplo = op_basicas()
- #print(ejemplo.ingresardato())
-#print(ejemplo.suma())
 
+# Creación de objeto 1
+ejemplo = op_basicas()
+print(ejemplo.ingresardato())
+print(ejemplo.suma())
+print(ejemplo.resta())
+print(ejemplo.producto())
+print(ejemplo.potencia())
+#Creación de objeto 2
 ejemplo = raiz()
 print(ejemplo.ingresardato())
 print(ejemplo.cuadrada())
+
 
